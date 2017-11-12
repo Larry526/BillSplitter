86150f5b8e3a17b573d036e6c59d446068856b49
Larry Luk <larryluk526@gmail.com>
2017-11-12 03:33:37 -0500

    final

--- BillSplitter/ViewController.m
+++ BillSplitter/ViewController.m
@@ -10,19 +10,42 @@
 
 @interface ViewController ()
 
+
+
+@property (weak, nonatomic) IBOutlet UITextField *billAmount;
+@property (weak, nonatomic) IBOutlet UILabel *splitAmount;
+@property (assign, nonatomic) NSInteger numberOfPeople;
+@property (weak, nonatomic) IBOutlet UILabel *sliderDisplay;
+@property (strong, nonatomic) NSNumberFormatter *numberFormatter;
+@property (weak, nonatomic) IBOutlet UISlider *peopleCount;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _numberFormatter = [[NSNumberFormatter alloc]init];
+    [_numberFormatter setNumberStyle:NSNumberFormatterCurrencyStyle];
+    [_numberFormatter setMaximumFractionDigits:2];
+    [_numberFormatter setRoundingMode:NSNumberFormatterRoundUp];
+    _numberOfPeople = _peopleCount.value;
+
+
 }
 
+- (IBAction)peopleCount:(UISlider *)sender {
+    self.numberOfPeople = sender.value;
+    self.sliderDisplay.text = [NSString stringWithFormat:@"%ld people splitting", self.numberOfPeople];
+    
+}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)calculateSplitAmount:(UIButton *)sender {
+//    NSNumber *splitCalc = [NSNumber numberWithFloat:[self.billAmount.text floatValue] / self.numberOfPeople];
+    NSNumber *splitCalc = @([self.billAmount.text floatValue] / self.numberOfPeople);
+    NSDecimalNumber *splitCalcDecimal = [NSDecimalNumber decimalNumberWithDecimal:[splitCalc decimalValue]];
+    self.splitAmount.text = [self.numberFormatter stringFromNumber:splitCalcDecimal];
+   
 }
 
 
